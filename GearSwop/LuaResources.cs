a3d171de404d0c566ce62949dd4ea6e200404f34
Shawn Gallagher <smokeythevwbus@hotmail.com>
2020-04-29 22:46:33 -0600

    Lua extraction shenanigans.

--- GearSwop/LuaResources.cs
+++ GearSwop/LuaResources.cs
@@ -1,31 +1,12 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using NLua;
 
 namespace GearSwop
 {
-    public class Program
+    public class LuaResources
     {
-        public static void Main(string[] args)
-        {
-            var items = LoadLuaResource("./Temp/items.lua");
-            var itemDescriptions = LoadLuaResource("./Temp/item_descriptions.lua");
-
-            var id = 22063;
-            Console.WriteLine(items[id]["en"]);
-            Console.WriteLine(itemDescriptions[id]["en"]);
-            CreateHostBuilder(args).Build().Run();
-        }
-
         private static Dictionary<int, Dictionary<string, string>> LoadLuaResource(string filepath)
         {
             var lua = new Lua();
@@ -46,8 +27,5 @@ namespace GearSwop
             return items;
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
     }
 }
